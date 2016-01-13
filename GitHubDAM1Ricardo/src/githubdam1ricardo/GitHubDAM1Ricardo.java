75b48c2256fa3b4df31d1ae257c1d2b91ea1c69b
dam1 <dam1@1DAM-09.IES.QUEVEDO-AD>
2016-01-13 13:01:56 +0100

    Prueba Git testing

--- GitHubDAM1Ricardo/src/githubdam1ricardo/GitHubDAM1Ricardo.java
+++ GitHubDAM1Ricardo/src/githubdam1ricardo/GitHubDAM1Ricardo.java
@@ -15,7 +15,7 @@ public class GitHubDAM1Ricardo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola ");
+        System.out.println("Hola que tal");
     }
     
 }
